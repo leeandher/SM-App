86789f0f6adb9d076c273ff7768228eaa941477e
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-06-27 19:41:46 -0400

    🐛 Fix credentials issue with firebase

---
+++ windsayl-functions/functions/index.js
@@ -0,0 +1,43 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://windsayl.firebaseio.com"
+});
+
+exports.getWaves = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("waves")
+    .get()
+    .then(data => {
+      const waves = [];
+      data.forEach(doc => waves.push(doc.data()));
+      return res.json(waves);
+    })
+    .catch(console.error);
+});
+
+exports.createWave = functions.https.onRequest((req, res) => {
+  const newWave = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+  };
+  console.log(JSON.stringify(newWave));
+
+  admin
+    .firestore()
+    .collection("waves")
+    .add(newWave)
+    .then(({ id }) =>
+      res.json({ message: `Wave (ID: ${id}) was successfully created.` })
+    )
+    .catch(err => {
+      res.status(500).json({ error: "Could not create new wave." });
+      console.error(err);
+    });
+});
