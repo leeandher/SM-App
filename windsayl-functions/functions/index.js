8c05d877e190ad1209f11aa5e516c6cde7f3ebb5
Leander Rodrigues <leandergrodrigues@gmail.com>
2019-06-27 20:59:18 -0400

    🐛 Fix auth error in firebase

--- windsayl-functions/functions/index.js
+++ windsayl-functions/functions/index.js
@@ -1,34 +1,46 @@
-const functions = require("firebase-functions");
+const app = require("express")();
+
+const firebase = require("firebase");
+const firebaseConfig = require("./etc/firebaseConfig.json");
+firebase.initializeApp(firebaseConfig);
+
 const admin = require("firebase-admin");
-
-const serviceAccount = require("./serviceAccountKey.json");
-
+const serviceAccount = require("./etc/serviceAccountKey.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://windsayl.firebaseio.com"
 });
 
-exports.getWaves = functions.https.onRequest((req, res) => {
+const functions = require("firebase-functions");
+
+// Get all waves
+app.get("/waves", (req, res) => {
   admin
     .firestore()
     .collection("waves")
+    .orderBy("createdAt", "desc")
     .get()
     .then(data => {
       const waves = [];
-      data.forEach(doc => waves.push(doc.data()));
+      data.forEach(doc =>
+        waves.push({
+          id: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        })
+      );
       return res.json(waves);
     })
     .catch(console.error);
 });
 
-exports.createWave = functions.https.onRequest((req, res) => {
-  if (req.method !== "POST") {
-    return res.status(400).json({ error: `${req.method} Method not allowed` });
-  }
+// Add a wave
+app.post("/waves", (req, res) => {
   const newWave = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString()
   };
   console.log(JSON.stringify(newWave));
 
@@ -44,3 +56,31 @@ exports.createWave = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    userHandle: req.body.userHandle
+  };
+
+  // TODO: Validate data
+  console.log(JSON.stringify(firebaseConfig));
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+      return res.status(201).json({
+        message: `User (ID: ${data.user.uid}) was successfully created.`
+      });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: `(${err.code}) Could not create new user.` });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
