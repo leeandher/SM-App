370bb0776a3d3a2c32023da014983af35efb92a7
Leander Rodrigues <me@leander.xyz>
2019-07-07 23:07:24 -0400

    ⚗ Implement the ripple functionality sans testing

--- windsayl-functions/functions/controllers/waveController.js
+++ windsayl-functions/functions/controllers/waveController.js
@@ -32,7 +32,8 @@ exports.createWave = catchErrors(
     const newWave = {
       body: req.body.body,
       handle: req.user.handle,
-      createdAt: new Date().toISOString()
+      createdAt: new Date().toISOString(),
+      rippleCount: 0
     }
     const { id } = await db.collection('waves').add(newWave)
     res.json({ message: `Wave (ID: ${id}) was successfully created` })
@@ -40,5 +41,36 @@ exports.createWave = catchErrors(
   (err, req, res) => {
     console.error(err)
     return res.status(500).json({ error: 'Could not create new wave' })
+  }
+)
+
+exports.rippleWave = catchErrors(
+  async (req, res) => {
+    const { waveId } = req.params
+    // 1. Get original wave, error if not found
+    const waveDoc = await db.doc(`/waves/${waveId}`).get()
+    // 2. If not found, error out
+    if (!waveDoc.exists) {
+      return res.status(404).json({ error: `Wave ${waveId} was not found!` })
+    }
+    // 3. Create the ripple
+    const newRipple = {
+      body: waveDoc.data().body,
+      handle: waveDoc.data().handle,
+      createdAt: new Date().toISOString(),
+      rippleHandle: req.user.handle
+    }
+    // 4. Save the ripple to the data store
+    await db.collection('waves').add(newWave)
+    // 5. Increase the rippleCount of the original wave
+    await db
+      .doc(`/waves/${waveId}`)
+      .update({ rippleCount: waveDoc.data().rippleCount + 1 })
+    // 6. Return the new ripple data
+    return res.json(newRipple)
+  },
+  (err, req, res) => {
+    console.error(err)
+    return res.status(500).json({ error: 'Could not ripple this wave' })
   }
 )
