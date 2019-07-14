d85869fde87e6b2e3c90259c393a34e7ce1885d1
Leander Rodrigues <me@leander.xyz>
2019-07-14 19:18:16 -0400

    🚚 Directory rename

---
+++ windsayl-client/pages/_app.js
@@ -0,0 +1,24 @@
+import App, { Container } from 'next/app'
+import Page from '../components/Page'
+
+class WindsaylApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+    return { pageProps }
+  }
+  render() {
+    const { Component, pageProps } = this.props
+    return (
+      <Container>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </Container>
+    )
+  }
+}
+
+export default WindsaylApp
