b5daa10940dae609cc9a057f314e37f4e4baa9a7
Leander Rodrigues <me@leander.xyz>
2019-07-19 13:50:56 -0400

    🐛 Enable CORS

--- windsayl-functions/functions/index.js
+++ windsayl-functions/functions/index.js
@@ -1,95 +1,98 @@
-const app = require('express')()
+const app = require("express")()
+
+// Allow cross-origin requests
+const cors = require("cors")
+app.use(cors({ origin: true }))
 
 // Initialize the Firebase App
-const firebase = require('firebase')
-const firebaseConfig = require('./etc/firebaseConfig.json')
+const firebase = require("firebase")
+const firebaseConfig = require("./etc/firebaseConfig.json")
 firebase.initializeApp(firebaseConfig)
 
-const admin = require('firebase-admin')
-const serviceAccount = require('./etc/serviceAccountKey.json')
+const admin = require("firebase-admin")
+const serviceAccount = require("./etc/serviceAccountKey.json")
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://windsayl.firebaseio.com'
+  databaseURL: "https://windsayl.firebaseio.com"
 })
 
-const db = admin.firestore()
-const functions = require('firebase-functions')
+const functions = require("firebase-functions")
 
-const authController = require('./controllers/authController')
-const waveController = require('./controllers/waveController')
-const userController = require('./controllers/userController')
-const notifController = require('./controllers/notifController')
-const triggerController = require('./controllers/triggerController')
+const authController = require("./controllers/authController")
+const waveController = require("./controllers/waveController")
+const userController = require("./controllers/userController")
+const notifController = require("./controllers/notifController")
+const triggerController = require("./controllers/triggerController")
 
 // Get all waves
-app.get('/wave', waveController.getWaves)
+app.get("/wave", waveController.getWaves)
 // Get a specific wave
-app.get('/wave/:waveId', waveController.getWave)
+app.get("/wave/:waveId", waveController.getWave)
 // Create a wave
-app.post('/wave', authController.verifyToken, waveController.createWave)
+app.post("/wave", authController.verifyToken, waveController.createWave)
 // Delete a wave
 app.delete(
-  '/wave/:waveId',
+  "/wave/:waveId",
   authController.verifyToken,
   waveController.deleteWave
 )
 // Create a ripple
 app.post(
-  '/wave/:waveId/ripple',
+  "/wave/:waveId/ripple",
   authController.verifyToken,
   waveController.createRipple
 )
 // Delete a ripple
 app.delete(
-  '/wave/:waveId/ripple',
+  "/wave/:waveId/ripple",
   authController.verifyToken,
   waveController.deleteRipple
 )
 // Create a comment
 app.post(
-  '/wave/:waveId/comment',
+  "/wave/:waveId/comment",
   authController.verifyToken,
   waveController.createComment
 )
 // Delete a comment
 app.delete(
-  '/wave/:waveId/comment/:commentId',
+  "/wave/:waveId/comment/:commentId",
   authController.verifyToken,
   waveController.deleteComment
 )
 // Splash a wave
 app.get(
-  '/wave/:waveId/splash',
+  "/wave/:waveId/splash",
   authController.verifyToken,
   waveController.createSplash
 )
 // Unsplash a wave
 app.delete(
-  '/wave/:waveId/splash',
+  "/wave/:waveId/splash",
   authController.verifyToken,
   waveController.deleteSplash
 )
 
 // Read notifications
 app.post(
-  '/notifications',
+  "/notifications",
   authController.verifyToken,
   userController.markNotifications
 )
 
 // Signup
-app.post('/signup', userController.signUp)
+app.post("/signup", userController.signUp)
 // Login
-app.post('/login', userController.login)
+app.post("/login", userController.login)
 // Get private user data
-app.get('/user/data', authController.verifyToken, userController.getUserPrivate)
+app.get("/user/data", authController.verifyToken, userController.getUserPrivate)
 // Get public  user data
-app.get('/user/:handle', userController.getUserPublic)
+app.get("/user/:handle", userController.getUserPublic)
 // Upload display picture
 // TODO: Fix and reimplement
-app.post('/user/image', authController.verifyToken, userController.uploadImage)
+app.post("/user/image", authController.verifyToken, userController.uploadImage)
 // Edit user details
-app.post('/user/edit', authController.verifyToken, userController.updateUser)
+app.post("/user/edit", authController.verifyToken, userController.updateUser)
 
 // Add routes at /api
 exports.api = functions.https.onRequest(app)
