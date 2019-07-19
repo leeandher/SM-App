ecc3e7a678cb172caf03a64c87caf6b54dd48b63
Leander Rodrigues <me@leander.xyz>
2019-07-19 13:17:06 -0400

    🚧 Implement homepage

--- windsayl-client/pages/index.js
+++ windsayl-client/pages/index.js
@@ -1,7 +1,30 @@
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-)
+import { useState, useEffect } from "react"
+import axios from "axios"
+import Grid from "@material-ui/core/Grid"
+import { FUNCTIONS_ENDPOINT } from "../constants"
 
-export default Index
+const Home = () => {
+  useEffect(() => {
+    async function getWaves() {
+      const { data } = await axios.get(`${FUNCTIONS_ENDPOINT}/wave`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+      console.log(data)
+    }
+    getWaves().catch(err => alert("EWWHAT"))
+  })
+  return (
+    <Grid container spacing={4}>
+      <Grid item sm={8} xs={12}>
+        <p>Content....</p>
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <p>Profile...</p>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Home
